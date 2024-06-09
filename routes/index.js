a14381cbc6cbf1109976ced47fe84a9b38a1e63c
RubsRafa <rubiarafaela2000@gmail.com>
2024-06-09 15:07:45 -0300

    fix: adjust exports to type module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+var router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/status', (req, res) => {
+  res.status(200).send('Servidor está funcionando corretamente');
+});
+
+export default router;
